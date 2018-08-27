fcf346bc6f0221a24be66a164187436a3e627802
OGordon100 <oliverg123@ntlworld.com>
2018-08-27 17:08:20 +0100

    Fix bugs, plot initial graphs

--- Importer.py
+++ Importer.py
@@ -1,4 +1,8 @@
 def importer():
+    import pandas as pd
+    from bs4 import BeautifulSoup
+    from urllib.request import Request, urlopen
+
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
     dframe = pd.DataFrame(data={"Title": [],
--- Metacritic.py
+++ Metacritic.py
@@ -1,12 +1,12 @@
 # Import stuff
 import pandas as pd
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
 import matplotlib.pyplot as plt
 import Importer
 
+# Parse metacritic for info
 #dframe = Importer()
 
+# Load metacritic info from file
 dframe = pd.read_csv("Database.csv")
 dframe["Release Date"] = pd.to_datetime(dframe["Release Date"])
 
@@ -15,17 +15,35 @@ dframe1 = dframe.groupby(["Title","Platform",dframe["Release Date"].dt.year]).me
 dframe2 = dframe1.groupby(["Release Date","Title"]).mean()
 
 # Get number releases for year, total or per console
-num_year = dframe1.groupby(["Platform","Release Date"]).count()
+num_year = dframe1.Metascore.groupby(["Platform","Release Date"]).count()
 total_num_year = dframe2.groupby("Release Date").count()
 
 # Get average release metascore for year, total or per console
-meta_year = dframe1.groupby(["Platform","Release Date"]).mean()
+meta_year = dframe1.Metascore.groupby(["Platform","Release Date"]).mean()
 total_meta_year = dframe2.groupby("Release Date").mean()
 
 # Number of games released for each platform
 num_console = dframe1.groupby("Platform").count()
 num_total = len(dframe)
 
-# Distribution of all metascores
+# Plot things
+plt.figure(0)
 plt.hist(dframe["Metascore"],bins=20)
- 
+plt.xlabel("Average Metascore")
+plt.ylabel("Number of Games")
+plt.figure(1)
+num_year.unstack().transpose().plot()
+plt.xlabel("Release Year")
+plt.ylabel("Number of Games")
+plt.figure(2)
+meta_year.unstack().transpose().plot()
+plt.xlabel("Release Year")
+plt.ylabel("Average Metascore")
+plt.figure(3)
+total_num_year.unstack().unstack().transpose().plot(legend=False)
+plt.xlabel("Release Year")
+plt.ylabel("Number of Games")
+plt.figure(4)
+total_meta_year.unstack().unstack().transpose().plot(legend=False)
+plt.xlabel("Release Year")
+plt.ylabel("Average Metascore")
